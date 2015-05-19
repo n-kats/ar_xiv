ff057ce1c8b11ce762d790c95d9aa9776b2381ec
n-kats <n-kats19890214@hotmail.co.jp>
2015-05-20 05:54:44 +0900

    integrate query.rb and request.rb

--- Gemfile
+++ Gemfile
@@ -3,7 +3,6 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in arxiv_reader.gemspec
 
 gem "nokogiri"
-gem 'bson'
 gem "guard"
 gem "guard-rspec"
 gem "wdm", ">=0.1.0" if Gem.win_platform?
--- lib/request.rb
+++ lib/request.rb
@@ -1,4 +1,75 @@
 module ArXiv
+  module QueryOperator
+    def and(query)
+      ComposedQuery.new("AND", self, query)
+    end
+
+    def or(query)
+      ComposedQuery.new("OR", self, query)
+    end
+    
+    def andnot(query)
+      ComposedQuery.new("ANDNOT", self, query)
+    end
+
+    # TODO: + ‚â - ‚ÌŽÀ‘•
+  end
+
+  # AND OR ANDOR ‚ðˆÓŽ¯‚µ‚È‚¢query
+  class Query
+    include QueryOperator
+    # @query‚Í
+    # {xx: [str,str,str]}
+    def initialize(key, value=nil)
+      if value==nil
+        case key
+        when String
+          @key = "all"
+          @value = [key]
+        when Array
+          @key = "all"
+          @value = key
+        when Hash
+          raise if key.keys.length != 1 # TODO: 1ˆÈŠO‚É‘Î‰ž
+          @key = key.keys.first
+          @value = (key[@key].is_a? Array) ? key[@key] : [key[@key]]
+        end
+      else
+        @key = key
+        @value = (key.is_a? Array) ? value : [value]
+      end
+    end
+
+    def to_query_string
+      "#{@key}:%28%22#{@value.join("%22+AND+%22")}%22%29"
+    end
+  end
+
+  class ComposedQuery
+    include QueryOperator
+    def initialize(str, *args)
+      case str
+      when String
+        @op = str
+        @requests = args
+      when Query 
+        @op = ""
+        @request = str
+      else
+        raise "Fail to make a Composed Request"
+      end
+    end
+
+    def to_query_string
+      if @op.empty?
+        @request.to_query_string
+      else
+        "%28#{@requests.map(&:to_query_string).join("%29+#{@op.upcase}+%28")}%29"
+      end
+    end
+
+  end
+
   class Request
 
     PARAMS = %w{search_query start max_results id_list sort_by sort_order}
